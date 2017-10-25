e1170c49ccbe86010870ce8f16e5cb3432623c0d
mike <zhtmike@gmail.com>
2017-10-25 22:31:07 +0800

    change for loop to javascript style

--- app/src/BankAccount/Chart.jsx
+++ app/src/BankAccount/Chart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Doughnut} from 'react-chartjs-2'
+import { Doughnut } from 'react-chartjs-2'
+import { range } from 'underscore'
 
 const options = {
   title: {
@@ -34,34 +35,31 @@ export default class BankAccountChart extends React.Component {
 //https://github.com/jerairrest/react-chartjs-2
 function convertListAccountData(listAccount) {
 
-  var i;
-  var chartObject = {};
-      chartObject.labels = [];
-      chartObject.datasets = [];
-  var tempData = {};
-      tempData.data = [];
-      tempData.backgroundColor = [];
+  return {
+    labels: [listAccount.map(function (val) {
+      return val.name;
+    })],
 
-  for (i = 0; i < listAccount.length; ++i) {
-    chartObject.labels.push(listAccount[i]['name']);
-    tempData.data.push(listAccount[i]['amount']);
-    tempData.backgroundColor.push(getColor(i * 3 % 17));
+    datasets: [{
+      data: listAccount.map(function (val) {
+        return val.amount;
+      }),
+      backgroundColor: range(listAccount.length).map(function (i) {
+        return getColor(i * 3 % 17);
+      })
+    }]
   }
-
-  chartObject.datasets.push(tempData);
-
-  return chartObject;
 }
 
 
 //Predefined color because chartJS does not provide any color scheme.........
 function getColor(index) {
   var color = ['#00BAF2', '#00BAF2', '#00a7d9', /* light blue */
-  '#E80C60', '#E80C60', '#d00a56', /* light pink */
-  '#9B26AF', '#9B26AF', '#8b229d', /* light purple */
-  '#E2D51A', '#E2D51A', '#E2D51A', /* med yellow */
-  '#FB301E', '#FB301E', '#e12b1b', /* med red */
-  '#00AE4D', '#00AE4D', '#00AE4D']; /* med green */
+    '#E80C60', '#E80C60', '#d00a56', /* light pink */
+    '#9B26AF', '#9B26AF', '#8b229d', /* light purple */
+    '#E2D51A', '#E2D51A', '#E2D51A', /* med yellow */
+    '#FB301E', '#FB301E', '#e12b1b', /* med red */
+    '#00AE4D', '#00AE4D', '#00AE4D']; /* med green */
 
   return color[index];
 }
