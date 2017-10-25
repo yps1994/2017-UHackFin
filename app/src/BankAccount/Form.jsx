a27718aa52189bada9074dc738b9130e170451fe
Peter Yeung <ypspak@gmail.com>
2017-10-25 19:29:58 +0800

    Decouple the Form in BankAccountSection.jsx by creating new file Form.jsx

---
+++ app/src/BankAccount/Form.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import { Form, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
+
+export default class BankAccountForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputAccountAmount: '',
+      inputAccountName: ''
+    };
+  }
+
+
+  createAccountObject = (name, amount) => {
+    return { name: this.name, amount: this.amount };
+  }
+
+
+  clearAccountFormInput = () => {
+    this.setState({
+      inputAccountAmount: '',
+      inputAccountName: ''
+    });
+  };
+
+
+  // Note we should use this.setState instead directly accessing the elements.
+  // See http://jamestw.logdown.com/posts/258005-reactjs-state
+  addAccount = () => {
+
+    if (this.getAccountAmountValidation() !== "success" || this.getAccountNameValidation() !== "success")
+      return;
+
+    const accountList = this.props.accountList;
+
+    accountList.push({
+      name: this.state.inputAccountName,
+      amount: this.state.inputAccountAmount
+    });
+
+    this.props.updateParentAccountList(accountList);
+
+    this.clearAccountFormInput();
+  }
+
+  
+  // It is necessarily to create a function acting as middleman between the components and the textbox values.
+  updateInputAccountName = (name) => {
+
+    this.setState({
+      inputAccountName: name.target.value
+    });
+  }
+
+
+  updateInputAccountAmount = (amount) => {
+    this.setState({
+      inputAccountAmount: amount.target.value
+    });
+  }
+
+
+  getAccountNameValidation = () => {
+
+    const accountList = this.props.accountList;
+    const inputAccountName = this.state.inputAccountName;
+
+    if (accountList == null || inputAccountName === '') return null;
+
+    var index = accountList.findIndex(i => i.name === inputAccountName);
+
+    if (index !== -1 || !inputAccountName) return 'error';
+    else return 'success';
+  }
+
+  
+  getAccountAmountValidation = () => {
+
+    if (this.state.inputAccountAmount === '') return null;
+    else if (isNumeric(this.state.inputAccountAmount)) return 'success';
+    else return 'error';
+  
+  }
+
+  
+  // Rendering section
+  render = () => {
+
+    const accountList = this.props.accountList;
+
+    return (
+      <div>
+          <Form>
+            <FormGroup controlId="formAccountName" validationState={this.getAccountNameValidation()}>
+
+              <ControlLabel>Bank account&#39;s name</ControlLabel>
+              <FormControl
+                type="text"
+                className="bankaccount-name-textbox"
+                placeholder="Enter account's name"
+                value={this.state.inputAccountName}
+                onChange={name => this.updateInputAccountName(name)}
+              />
+              <FormControl.Feedback/>
+              <HelpBlock>Your account&#39;s name should be different from the above table. </HelpBlock>
+            </FormGroup>
+
+            <FormGroup controlId="formAccountAmount" validationState={this.getAccountAmountValidation()}>
+              <ControlLabel>Amount</ControlLabel>
+              <FormControl
+                type="text"
+                className="bankaccount-amount-textbox"
+                placeholder="Amount"
+                value={this.state.inputAccountAmount}
+                onChange={amount => this.updateInputAccountAmount(amount)}
+              />
+              <FormControl.Feedback/>
+              <HelpBlock>The amount should be greater than or equal to 0. </HelpBlock>
+            </FormGroup>
+          </Form>
+
+          <button className="addButton" onClick={this.addAccount}>Add account</button>
+      </div>
+    );
+  }
+}
+
+
+function isNumeric(amount) {
+
+  if (isNaN(amount) || !isFinite(amount) || amount < 0) return false;
+
+  return true;
+
+}
--- app/src/BankAccountSection.jsx
+++ app/src/BankAccountSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Form, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
 
 import BankAccountTable from './BankAccount/Table'
 import BankAccountChart from './BankAccount/Chart'
+import BankAccountForm from './BankAccount/Form'
 // TODO:    
 //          1) Add delete button so that the list can be removed at any time
 
@@ -11,94 +11,13 @@ import BankAccountChart from './BankAccount/Chart'
 export default class BankAccountSection extends React.Component {
   constructor(props) {
     super(props);
-    //this.onAfterSaveCell = this.onAfterSaveCell.bind(this);
-    // save data in state
-    this.state = {
-      inputAccountName: '',
-      inputAccountAmount: '',
-      accountList: [],
-    };
+    this.state = { accountList: [] };
   }
 
 
-  editAccountAmount = (row, amount) => {
-    this.accountList[row].amount.setState({ amount });
-  }
-
-
-  createAccountObject = (name, amount) => {
-    return { name: this.name, amount: this.amount };
-  }
-
-
-  clearAccountFormInput = () => {
-    this.setState({
-      inputAccountAmount: '',
-      inputAccountName: ''
-    });
-  }
-
-
-  // Note we should use this.setState instead directly accessing the elements.
-  // See http://jamestw.logdown.com/posts/258005-reactjs-state
-  addAccount = () => {
-
-    if (this.getAccountAmountValidation() !== "success" || this.getAccountNameValidation() !== "success")
-      return;
-
-    const accountList = this.state.accountList;
-
-    this.state.accountList.push({
-      name: this.state.inputAccountName,
-      amount: this.state.inputAccountAmount
-    });
-
-    this.setState({ accountList });
-
-    this.clearAccountFormInput();
-  }
-
-
-  // It is necessarily to create a function acting as middleman between the components and the textbox values.
-  updateInputAccountName = (name) => {
-    this.setState({
-      inputAccountName: name.target.value
-    });
-  }
-
-
-  updateInputAccountAmount = (amount) => {
-    this.setState({
-      inputAccountAmount: amount.target.value
-    });
-  }
-
-
+  //This function is a middle-man between different DOM.
   updateBankAccountList = (updatedAccountList) => {
     this.setState({accountList: updatedAccountList});
-  }
-
-
-  getAccountNameValidation = () => {
-
-    const accountList = this.state.accountList;
-    const inputAccountName = this.state.inputAccountName;
-
-    if (accountList == null || inputAccountName === '') return null;
-
-    var index = accountList.findIndex(i => i.name === inputAccountName);
-
-    if (index !== -1 || !inputAccountName) return 'error';
-    else return 'success';
-  }
-
-  
-  getAccountAmountValidation = () => {
-
-    if (this.state.inputAccountAmount === '') return null;
-    else if (isNumeric(this.state.inputAccountAmount)) return 'success';
-    else return 'error';
-  
   }
 
   
@@ -111,37 +30,7 @@ export default class BankAccountSection extends React.Component {
       <div>
         <div className="col-md-6 divider-right-4px">
           <BankAccountTable accountList={accountList} updateParentAccountList={this.updateBankAccountList} />
-
-          <Form>
-            <FormGroup controlId="formAccountName" validationState={this.getAccountNameValidation()}>
-
-              <ControlLabel>Bank account&#39;s name</ControlLabel>
-              <FormControl
-                type="text"
-                className="bankaccount-name-textbox"
-                placeholder="Enter account's name"
-                value={this.state.inputAccountName}
-                onChange={name => this.updateInputAccountName(name)}
-              />
-              <FormControl.Feedback/>
-              <HelpBlock>Your account&#39;s name should be different from the above table. </HelpBlock>
-            </FormGroup>
-
-            <FormGroup controlId="formAccountAmount" validationState={this.getAccountAmountValidation()}>
-              <ControlLabel>Amount</ControlLabel>
-              <FormControl
-                type="text"
-                className="bankaccount-amount-textbox"
-                placeholder="Amount"
-                value={this.state.inputAccountAmount}
-                onChange={amount => this.updateInputAccountAmount(amount)}
-              />
-              <FormControl.Feedback/>
-              <HelpBlock>The amount should be greater than or equal to 0. </HelpBlock>
-            </FormGroup>
-          </Form>
-
-          <button className="addButton" onClick={this.addAccount}>Add account</button>
+          <BankAccountForm accountList={accountList} updateParentAccountList={this.updateBankAccountList} />
         </div>
         <div className="col-md-6">
           <BankAccountChart accountList={accountList} />
@@ -149,12 +38,4 @@ export default class BankAccountSection extends React.Component {
       </div>
     );
   }
-}
-
-
-function isNumeric(amount) {
-
-  if (isNaN(amount) || !isFinite(amount) || amount < 0) return false;
-
-  return true;
 }
