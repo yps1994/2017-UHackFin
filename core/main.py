ceee13647f9ce7bcb05c76aba61b59ca2e6ffa8e
mike <zhtmike@gmail.com>
2017-11-04 19:43:19 +0800

    add stat wrapper

--- core/main.py
+++ core/main.py
@@ -2,15 +2,13 @@ import flask
 import flask_restful
 
 import utils.predict
-from routes import index, risk_api
+from routes import index, stat
 
 
 app = flask.Flask(__name__)
-predictor = utils.predict.Predictor(app, "data/null.pkl")
 
 api = flask_restful.Api(app, catch_all_404s=True)
 api.add_resource(index.Index, '/')
-api.add_resource(risk_api.RiskAPI, '/risk',
-                 resource_class_kwargs={'predictor': predictor})
+api.add_resource(stat.StatAPI, '/stat/<string:symbol>')
 if __name__ == '__main__':
     app.run()
