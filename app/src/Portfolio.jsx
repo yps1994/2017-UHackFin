7a84f092a38f8ff5e981b8055cb275be30b761ec
Peter Yeung <ypspak@gmail.com>
2017-11-05 11:02:44 +0800

    Add demonstration data.

--- app/src/Portfolio.jsx
+++ app/src/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+
 import {_} from 'underscore';
+
 import BankAccountSection from './BankAccountSection';
 import StockSection from './StockSection';
 import SummarySection from './SummarySection';
@@ -12,7 +14,8 @@ export default class Portfolio extends React.Component {
     super(props);
     this.state = { 
       user_id: 1,
-      portfolio_amount: [193000, 200000, 210000, 200300],
+      portfolio_amount: [193000, 200000, 210000, 200300, 206500],
+      portfolio_date: ["2017-10-07","2017-10-14","2017-10-21","2017-10-28", "2017-11-04"],
       accountList: [
         {
           name: 'Bank Of China',
@@ -32,19 +35,29 @@ export default class Portfolio extends React.Component {
         currentPrice: 99.15,
         earn: 200,
         amount: 49375,
-        hotness:0.087
+        hotness:0.087,
+        max_drawdown: 13.0869,
+        beta: 0.0018,
+        variance: 15.7128,
+        average_per: 6.9307,
+        mean_ratio: 27.5244
       },
       {
         id: 2,
-        code: '0700.HK',
-        name: 'TENCENT',
+        code: '0005.HK',
+        name: 'HSBC HOLDINGS',
         tradingDay: '2017-10-30',
-        shares: 100,
-        buyingPrice: 349,
-        currentPrice: 368,
-        earn: 1900,
-        amount: 34900,
-        hotness:0.0117
+        shares: 500,
+        buyingPrice: 77.05,
+        currentPrice: 75.6,
+        earn: -725,
+        amount: 38525,
+        hotness:0.087,
+        max_drawdown: 10.4348,
+        beta: 0.0028,
+        variance: 35.3712,
+        average_per: 6.9307,
+        mean_ratio: 22.8506
       },
       {
         id: 3,
@@ -56,7 +69,12 @@ export default class Portfolio extends React.Component {
         currentPrice: 38.55,
         earn: -50,
         amount: 38600,
-        hotness:0.093
+        hotness:0.093,
+        max_drawdown: 7.9125,
+        beta: 0.0021,
+        variance: 17.2304,
+        average_per: 11.8811,
+        mean_ratio: 19.7962
       }]
     };
   }
@@ -64,6 +82,7 @@ export default class Portfolio extends React.Component {
   // This function is a middle-man between different DOM.
   updateBankAccountList = (updatedAccountList) => {
     this.setState({accountList: updatedAccountList});
+    this.updateCurrentDay();
   }
 
   updateStockList = (updatedStockList) => {
@@ -87,7 +106,41 @@ export default class Portfolio extends React.Component {
           console.log(error);
         });
     });
+
+    this.updateCurrentDay();
   }
+
+  updateCurrentDay = () => {
+    /*var investedAmount = this.obtainTotalInvestedAmount(this.state.stockList);
+    var bankAmount = this.obtainTotalBankAmount(this.state.accountList);
+
+    const portfolioAmount = this.state.portfolio_amount;
+    portfolioAmount[4] = investedAmount + bankAmount;
+
+    this.setState({portfolio_amount: portfolioAmount});*/
+    
+  }
+
+  obtainTotalInvestedAmount = (arr) => {
+    
+    return arr.reduce((total, obj) => {
+      if (typeof obj['amount'] === 'string') {
+        return total + Number(obj['amount']);
+      }
+      return total + obj['amount'];
+    }, 0);
+  }
+
+  obtainTotalBankAmount = (arr) => {
+    
+    return arr.reduce((total, obj) => {
+      if (typeof obj['amount'] === 'string') {
+        return total + Number(obj['amount']);
+      }
+      return total + obj['amount'];
+    }, 0);
+  }
+
 
   // Rendering section
   render = () => {
@@ -95,12 +148,13 @@ export default class Portfolio extends React.Component {
     const accountList = this.state.accountList;
     const stockList = this.state.stockList;
     const user_id = this.state.user_id;
-    const portfolio_sum = this.state.portfolio_sum;
+    const portfolio_amount = this.state.portfolio_amount;
+    const portfolio_date = this.state.portfolio_date;
     return (
       <div>
           <BankAccountSection user_id={user_id} accountList={accountList} updateParentAccountList={this.updateBankAccountList} />
           <StockSection user_id={user_id} stockList={stockList} updateParentStockList={this.updateStockList} />
-          
+          <SummarySection user_id={user_id} historyPortfolioAmount={portfolio_amount} historyPortfolioDate={portfolio_date} />
       </div>
     );
   }
