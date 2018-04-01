0ff4cfbb76575691ee25cc85cd4325214da8fa3d
Peter Yeung <ypspak@gmail.com>
2018-04-02 01:22:12 +0800

    Add button for injecting examples.

--- app/src/Portfolio.jsx
+++ app/src/Portfolio.jsx
@@ -13,70 +13,12 @@ import SummarySection from './SummarySection';
 export default class Portfolio extends React.Component {
   constructor (props) {
     super(props);
-    this.state = { 
-      user_id: 1,
-      portfolio_amount: [193000, 200000, 210000, 200300, 206500],
-      portfolio_date: ["2017-10-07","2017-10-14","2017-10-21","2017-10-28", "2017-11-16"],
-      accountList: [
-        {
-          name: 'Bank Of China',
-          amount: 50000
-        },
-        {
-          name: 'HSBC',
-          amount: 30000
-        }],
-      stockList: [{
-        id: 1,
-        code: '0001.HK',
-        name: 'CKH HOLDINGS',
-        tradingDay: '2017-10-31',
-        shares: 500,
-        buyingPrice: 98.75,
-        currentPrice: 99.15,
-        earn: 200,
-        amount: 49375,
-        hotness:0.087,
-        max_drawdown: 13.0869,
-        beta: 0.0018,
-        variance: 15.7128,
-        average_per: 6.9307,
-        mean_ratio: 27.5244
-      },
-      {
-        id: 2,
-        code: '0005.HK',
-        name: 'HSBC HOLDINGS',
-        tradingDay: '2017-10-30',
-        shares: 500,
-        buyingPrice: 77.05,
-        currentPrice: 75.6,
-        earn: -725,
-        amount: 38525,
-        hotness:0.087,
-        max_drawdown: 10.4348,
-        beta: 0.0028,
-        variance: 35.3712,
-        average_per: 6.9307,
-        mean_ratio: 22.8506
-      },
-      {
-        id: 3,
-        code: '2388.HK',
-        name: 'BOC HONG KONG',
-        tradingDay: '2017-10-13',
-        shares: 1000,
-        buyingPrice: 38.6,
-        currentPrice: 38.55,
-        earn: -50,
-        amount: 38600,
-        hotness:0.093,
-        max_drawdown: 7.9125,
-        beta: 0.0021,
-        variance: 17.2304,
-        average_per: 11.8811,
-        mean_ratio: 19.7962
-      }]
+    this.state = {  
+      user_id: 1, 
+      accountList: [], 
+      stockList: [],
+      portfolio_amount: [],
+      portfolio_date: []
     };
   }
 
@@ -142,6 +84,73 @@ export default class Portfolio extends React.Component {
     }, 0);
   }
 
+  exampleDataInjection = () => {
+    this.setState({ 
+      user_id: 1,
+      portfolio_amount: [193000, 200000, 210000, 200300, 206500],
+      portfolio_date: ["2017-10-07","2017-10-14","2017-10-21","2017-10-28", "2017-11-16"],
+      accountList: [
+        {
+          name: 'Bank Of China',
+          amount: 50000
+        },
+        {
+          name: 'HSBC',
+          amount: 30000
+        }],
+      stockList: [{
+        id: 1,
+        code: '0001.HK',
+        name: 'CKH HOLDINGS',
+        tradingDay: '2017-10-31',
+        shares: 500,
+        buyingPrice: 98.75,
+        currentPrice: 99.15,
+        earn: 200,
+        amount: 49375,
+        hotness:0.087,
+        max_drawdown: 13.0869,
+        beta: 0.0018,
+        variance: 15.7128,
+        average_per: 6.9307,
+        mean_ratio: 27.5244
+      },
+      {
+        id: 2,
+        code: '0005.HK',
+        name: 'HSBC HOLDINGS',
+        tradingDay: '2017-10-30',
+        shares: 500,
+        buyingPrice: 77.05,
+        currentPrice: 75.6,
+        earn: -725,
+        amount: 38525,
+        hotness:0.087,
+        max_drawdown: 10.4348,
+        beta: 0.0028,
+        variance: 35.3712,
+        average_per: 6.9307,
+        mean_ratio: 22.8506
+      },
+      {
+        id: 3,
+        code: '2388.HK',
+        name: 'BOC HONG KONG',
+        tradingDay: '2017-10-13',
+        shares: 1000,
+        buyingPrice: 38.6,
+        currentPrice: 38.55,
+        earn: -50,
+        amount: 38600,
+        hotness:0.093,
+        max_drawdown: 7.9125,
+        beta: 0.0021,
+        variance: 17.2304,
+        average_per: 11.8811,
+        mean_ratio: 19.7962
+      }]
+    });
+  }
 
   // Rendering section
   render = () => {
@@ -153,6 +162,7 @@ export default class Portfolio extends React.Component {
     const portfolio_date = this.state.portfolio_date;
     return (
       <div>
+          <button style={{float: "right"}} type="button" class="btn btn-info" onClick={this.exampleDataInjection}> Demonstration Button </button>
           <BankAccountSection user_id={user_id} accountList={accountList} updateParentAccountList={this.updateBankAccountList} />
           <StockSection user_id={user_id} stockList={stockList} updateParentStockList={this.updateStockList} />
           <StockSimilaritySection user_id={user_id} stockList={stockList} />
