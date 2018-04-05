840ad624622d019bc683444506d2cecfb1a9c2b2
mike <zhtmike@gmail.com>
2018-04-05 22:53:49 +0800

    Update build.sh
    - using built-in script to create tables

--- build.sh
+++ build.sh
@@ -6,36 +6,32 @@ export MYSQL_DATABASE=
 export MYSQL_USER=
 export MYSQL_PASSWORD=
 export MYSQL_PORT=
-export MYSQL_HOST=  # Bind with your local ip or virtualbox image ip, depends on your setting
+export MYSQL_HOST=
 
 # ==================================== 
 # DO NOT MODIFY ANYTHING BELOW
 # ==================================== 
+
+# container name
+container_db=sqldb
+
 if ! [ -x "$(which docker)" ]; then
     echo "Docker is not installed. Aborted. "
     exit 1
 fi
 
-# Set up the database and wait for some seconds until the container is started
-echo "Setting up database...";
-docker run --name sqldb -p ${MYSQL_PORT}:3306  \
- -e MYSQL_RANDOM_ROOT_PASSWORD=yes             \
- -e MYSQL_DATABASE=${MYSQL_DATABASE}           \
- -e MYSQL_USER=${MYSQL_USER}                   \
- -e MYSQL_PASSWORD=${MYSQL_PASSWORD}           \
- -d mysql && sleep 20
-
-# Set up the table
-echo "Setting up table...";
-docker run --rm --name setupdb --link sqldb:mysql                   \
- -v "$PWD/script":/script mysql sh -c                               \
- "mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -D ${MYSQL_DATABASE}   \
- -h ${MYSQL_HOST} -P ${MYSQL_PORT} < /script/sql/initdb.sql"
+# Set up the database
+echo "Setting up database and table";
+docker container inspect ${container_db} > /dev/null 2>&1 || docker run --name ${container_db} -p ${MYSQL_PORT}:3306  \
+ --mount type=bind,src="$(pwd)/database/scripts/",dst="/docker-entrypoint-initdb.d/"    \
+ -e MYSQL_DATABASE=${MYSQL_DATABASE}    \
+ -e MYSQL_USER=${MYSQL_USER}            \
+ -e MYSQL_PASSWORD=${MYSQL_PASSWORD}    \
+ -d mysql/mysql-server:latest
 
 # ==================================== 
 # Setting up the backend server
 # ==================================== 
-# We use local Nodejs for development, will be moved to Docker/nodejs for deployment (if really necessary)
 echo "Setting up backend...";
 if ! [ -x "$(which node)" ]; then
     echo "Node is not installed. Aborted. "
