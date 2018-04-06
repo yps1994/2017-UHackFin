d01647acf1331ead5f4bf0c433c6f7901755b5a4
mike <zhtmike@gmail.com>
2018-04-06 20:43:19 +0800

    organize directory and update script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 
 # SQL settings
-export MYSQL_DATABASE=stock
-export MYSQL_USER=backend
-export MYSQL_PASSWORD=backend#1
-export MYSQL_PORT=3306
+export MYSQL_DATABASE=
+export MYSQL_USER=
+export MYSQL_PASSWORD=
+export MYSQL_PORT=
 export MYSQL_HOST=
 
 # ==================================== 
@@ -20,18 +20,19 @@ if ! [ -x "$(which docker)" ]; then
     exit 1
 fi
 
-# Set up the database
+# Set up the database and start the server
 echo "Setting up database and table";
 docker create --name ${container_db} -p ${MYSQL_PORT}:3306  \
  --mount type=bind,src="$(pwd)/database/scripts/",dst="/docker-entrypoint-initdb.d/"    \
  -e MYSQL_DATABASE=${MYSQL_DATABASE}    \
  -e MYSQL_USER=${MYSQL_USER}            \
  -e MYSQL_PASSWORD=${MYSQL_PASSWORD}    \
- mysql/mysql-server:latest
+ mysql/mysql-server:latest > /dev/null
+
+docker cp $(pwd)/database/csv/*.csv ${container_db}:/var/lib/mysql-files/
 
 echo "Starting MySQL service";
-docker cp $(pwd)/database/csv/*.csv ${container_db}:/var/lib/mysql-files/
-docker start ${container_db}
+docker start ${container_db} > /dev/null
 
 # ==================================== 
 # Setting up
--- script/sql/testdb.sql
+++
@@ -1,3 +0,0 @@
-# For test purpose
-INSERT INTO stocks_raw (id, date, high, low, open, close) VALUES ('0700', '2017-10-20', 350.6, 348.0, 348.0, 349.6);
-INSERT INTO stocks_raw (id, date, high, low, open, close) VALUES ('0005', '2017-10-20', 79.6, 76.4, 76.5, 76.5);
