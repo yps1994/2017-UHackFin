0cfd68d2beb3ffa689f96842b22ee41f8f1debbb
mike <zhtmike@gmail.com>
2018-04-06 11:44:17 +0800

    insert stock detail when creating db

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 set -e
 
 # SQL settings
-export MYSQL_DATABASE=
-export MYSQL_USER=
-export MYSQL_PASSWORD=
-export MYSQL_PORT=
+export MYSQL_DATABASE=stock
+export MYSQL_USER=backend
+export MYSQL_PASSWORD=backend#1
+export MYSQL_PORT=3306
 export MYSQL_HOST=
 
 # ==================================== 
@@ -22,12 +22,20 @@ fi
 
 # Set up the database
 echo "Setting up database and table";
-docker container inspect ${container_db} > /dev/null 2>&1 || docker run --name ${container_db} -p ${MYSQL_PORT}:3306  \
+docker create --name ${container_db} -p ${MYSQL_PORT}:3306  \
  --mount type=bind,src="$(pwd)/database/scripts/",dst="/docker-entrypoint-initdb.d/"    \
  -e MYSQL_DATABASE=${MYSQL_DATABASE}    \
  -e MYSQL_USER=${MYSQL_USER}            \
  -e MYSQL_PASSWORD=${MYSQL_PASSWORD}    \
- -d mysql/mysql-server:latest
+ mysql/mysql-server:latest
+
+echo "Starting MySQL service";
+docker cp $(pwd)/database/csv/*.csv ${container_db}:/var/lib/mysql-files/
+docker start ${container_db}
+
+# ==================================== 
+# Setting up
+# ==================================== 
 
 # ==================================== 
 # Setting up the backend server
@@ -37,16 +45,3 @@ if ! [ -x "$(which node)" ]; then
     echo "Node is not installed. Aborted. "
     exit 1
 fi
-
-if ! [ -x "$(which npm)" ]; then
-    echo "npm is not installed. Aborted. "
-    exit 1
-fi
-
-cd ./backend && npm install && cd ../
-
-# ==================================== 
-# Setting up the frontend server
-# ==================================== 
-echo "Setting up frontend...";
-cd ./app && npm install && cd ../
